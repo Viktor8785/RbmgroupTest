e1dd08b01bc6bfa4c2bf52085716ddb926b67456
Viktor8785 <v117@list.ru>
2024-02-28 14:07:59 +0300

    add watermark

---
+++ js/object-object.js
@@ -0,0 +1,1 @@
+export const object={imagesCount:5,imagesPath:["../img/sale/object1-desktop.jpg","../img/sale/object2-desktop.jpg","../img/sale/object3-desktop.jpg","../img/sale/object4-desktop.jpg","../img/sale/object5-desktop.jpg"]};
