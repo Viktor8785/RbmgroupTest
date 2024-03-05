e0a0e69bd62163cc1bd4cf5b4843af8ce5a4bca2
Viktor8785 <v117@list.ru>
2024-03-05 13:09:53 +0300

    change img path

--- js/balloon-template.js
+++ js/balloon-template.js
@@ -1,5 +1,5 @@
 export const balloonCard =
-` <img class="balloon-img" src="../img/Card1-desktop.jpg">
+` <img class="balloon-img" src="./img/Card1-desktop.jpg">
   <p class="balloon-title">Помещение <span class="balloon-dot">\u00B7<\span><span class="balloon-span"> 120 м<sup class="balloon-sup">2<\sup><\span></p>
   <p class="balloon-address">Матросова 66А</p>
   <p class="ballon-price"></p>`
